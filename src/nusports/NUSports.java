4991182baa1a491c5382c70f4ad1e8108129104d
PtrTeixeira <ptrTeixeira@gmail.com>
2015-10-13 20:57:52 -0400

    Added back files
    Adding back the files that were removed when I reset the cache. I hate
    futzing up the log like this, but at least I finally kind-of know how to
    use .gitignore!

---
+++ src/nusports/NUSports.java
@@ -0,0 +1,114 @@
+package nusports;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Peter
+ * @version 0.1
+ */
+public class NUSports extends Application {
+    private final TableView actionTarget = new TableView();
+    private final ComboBox<String> sports = new ComboBox<>();
+    private final ComboBox<String> options = new ComboBox();
+    private final Text err = new Text();
+    
+    private final OutputGenerator og = new OutputGenerator(actionTarget, err);
+    
+    
+    // Set up stage: add table, side bar
+    public void start(Stage primaryStage) {
+        BorderPane border = new BorderPane();
+        
+        VBox leftMenu = this.addSideBar();
+        border.setLeft(leftMenu);
+        
+        this.setResponseText();
+        // actionTarget.set -- Change how the table behaves under sorts
+        border.setCenter(actionTarget);
+        
+        Scene scene = new Scene(border, 800, 800);
+        scene.getStylesheets().add(
+                getClass().getResource("OutputStyles.css").toExternalForm());
+        scene.setOnKeyReleased((KeyEvent ke) -> {
+            if (ke.getCode()==KeyCode.F5) {
+                this.reload();
+            }
+        });
+        
+        primaryStage.setTitle("NU Sports");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    // Add the side bar containing a header, the controls, and an error text
+    private VBox addSideBar() {
+        // The side bar
+        VBox vbox = new VBox();
+        vbox.setId("sidebar");
+        vbox.setPadding(new Insets(10));
+        vbox.setSpacing(8);
+        
+        // Header for the side bar
+        Text title = new Text("Settings");
+        title.setId("title");
+        
+        // controls: sports options
+        sports.getItems().addAll("Baseball", 
+                                 "Men's Basketball", 
+                                 "Men's Soccer", 
+                                 "Women's Basketball", 
+                                 "Women's Soccer", 
+                                 "Volleyball");
+        sports.setValue("Baseball");
+        sports.setOnAction((ActionEvent e) -> {
+            setResponseText();
+        });
+        // Controls: display options
+        options.getItems().addAll("League Standings",
+                                   "Schedule/Results");
+        options.setValue("League Standings");
+        options.setOnAction((ActionEvent e) -> {
+            setResponseText();
+        });
+        
+        // The error bar
+        err.setId("error");
+        
+        // Add children to the side bar
+        vbox.getChildren().add(title);
+        vbox.getChildren().add(sports);
+        vbox.getChildren().add(options);
+        vbox.getChildren().add(err);
+        
+        
+        return vbox;
+    }
+    
+    // Query ouput generator to set the response text
+    private void setResponseText() {
+        og.resetTable(this.sports.getValue(), this.options.getValue());
+    }
+    
+    private void reload() {
+        og.reloadCurrent(this.sports.getValue(), this.options.getValue());
+    }
+}
