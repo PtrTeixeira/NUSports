93c1bae742f928e02d889001f8d03f9168a5a650
Peter Teixeira <ptrTeixeira@gmail.com>
2017-06-27 18:18:45 -0400

    Add loading bar
    Add a loading bar to the bottom corner of the application when the UI is
    loading new data into the table. This makes the UI much more obviously
    responsive, rather than just having it appear to not do anything and
    suddenly load more things into the table. I would prefer it if the table
    went away while loading; will look into it.

--- src/main/kotlin/com/github/ptrteixeira/nusports/view/BodyTab.kt
+++ src/main/kotlin/com/github/ptrteixeira/nusports/view/BodyTab.kt
@@ -21,23 +21,27 @@
  */
 package com.github.ptrteixeira.nusports.view
 
+import javafx.beans.property.BooleanProperty
 import javafx.beans.property.StringProperty
 import javafx.scene.paint.Color
 import tornadofx.View
 import tornadofx.borderpane
+import tornadofx.bottom
 import tornadofx.center
 import tornadofx.combobox
 import tornadofx.left
+import tornadofx.progressbar
 import tornadofx.text
 import tornadofx.vbox
+import tornadofx.visibleWhen
 
 class BodyTab(
     private val sports: List<String>,
     private val centerContents: View,
     selectedItem: StringProperty,
-    errorText: StringProperty
+    errorText: StringProperty,
+    isLoading: BooleanProperty
 ) : View() {
-
     override val root = borderpane {
         left {
             vbox {
@@ -54,6 +58,12 @@ class BodyTab(
 
         center {
             add(centerContents)
+        }
+
+        bottom {
+            progressbar {
+                visibleWhen(isLoading)
+            }
         }
     }
 }
